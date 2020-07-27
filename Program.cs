191a15371720474138b9d0d74ab69707bf5b0eac
chuck francis <chuck_francis10@yahoo.com>
2020-07-27 13:50:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoreBucketsMoreFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare All The Things
+            // (Usually it's a good idea to declare them at the beginning of the program)
+            int butterflies, beetles, bugs;
+            string color, size, shape, thing;
+            double number;
+
+            //Now give a couple of them some values
+            butterflies = 2;
+            beetles = 4;
+
+            bugs = butterflies + beetles;
+            Console.WriteLine("There are only  " + butterflies + butterflies);
+            Console.WriteLine("but " + bugs + " bugs total.");
+
+            Console.WriteLine("Uh oh, my dog ate one.");
+            butterflies--;
+            Console.WriteLine("Now there only " + butterflies + " butterlies left.");
+            Console.WriteLine("But still " + bugs + " bugs left, wait a minute!!!");
+            Console.WriteLine("Maybe I just counted wrong the first time...");
+        }
+    }
+}
+    
